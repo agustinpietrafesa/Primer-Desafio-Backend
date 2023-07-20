3147cf37340c7ccc8c9ea098b7fe7b8a8c0b6172
Agustin Pietrafesa <ahpietrafesa@gmail.com>
2023-07-20 12:15:53 -0300

    FIn del tercer desafio con express

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const managerProductos = new ProductManager()
 
 const app = express()
 
-const port = 3000
+const port = 8080
 
 const allProducts = async () => {
     try {
@@ -22,29 +22,55 @@ const allProducts = async () => {
 
         // await managerProductos.addProduct('producto Prueba 4',  'Este es un producto de prueba', 600, 'Sin imagen', 'abc4', 25 )
 
-        // const products2 = await managerProductos.getProducts()
-        // console.log(products2)
-        
-        // await managerProductos.getProductById(1)
+        // await managerProductos.addProduct('producto Prueba 5',  'Este es un producto de prueba', 300, 'Sin imagen', 'abc5', 25 )
 
-        // await managerProductos.updateProduct(1, 'title' , 'nuevo titulo')
-        
-        // await managerProductos.updateProduct(1, 'price' , 'nuevo precio')
+        // await managerProductos.addProduct('producto Prueba 6',  'Este es un producto de prueba', 400, 'Sin imagen', 'abc6', 25 )
 
-        // await managerProductos.deleteProduct(1)
+        // await managerProductos.addProduct('producto Prueba 7',  'Este es un producto de prueba', 500, 'Sin imagen', 'abc7', 25 )
 
-        // const products1 = await managerProductos.getProducts()
-        // console.log(products1)
+        // await managerProductos.addProduct('producto Prueba 8',  'Este es un producto de prueba', 600, 'Sin imagen', 'abc8', 25 )
+
+        // await managerProductos.addProduct('producto Prueba 9',  'Este es un producto de prueba', 300, 'Sin imagen', 'abc9', 25 )
+
+        // await managerProductos.addProduct('producto Prueba 10',  'Este es un producto de prueba', 400, 'Sin imagen', 'abc10', 25 )
+
+        // await managerProductos.addProduct('producto Prueba 11',  'Este es un producto de prueba', 500, 'Sin imagen', 'abc11', 25 )
+
+        // await managerProductos.addProduct('producto Prueba 12',  'Este es un producto de prueba', 600, 'Sin imagen', 'abc12', 25 )
 
     } catch (error) {
         console.log(error)
     }
     }
 
-    app.get('/', (req, res) => {
-        const allProducts = managerProductos.getProducts()
-        res.send({allProducts})
+    app.get('/products', async (req, res) => {
+        try {
+            const { limit } = req.query
+            const allProducts = await managerProductos.getProducts()
+            const productsFilter = allProducts.slice(0, limit || 10)
+            res.json({Productos: productsFilter})
+            
+        } catch (error) {
+            console.log(error)
+        }
     })
+
+    app.get('/products/:id', async (req, res) => {
+        try {
+            const { id } = req.params
+            const allProducts = await managerProductos.getProducts()
+            const productFind = allProducts.find(product => product.id === Number(id))
+            if(!productFind){
+                res.send(`No se encontro un producto con el id ${id}`)
+            }else{
+                return res.json({Producto: productFind})
+            }
+            
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
 
     app.listen(port, () => {
         console.log(`Servidor corriendo en el puerto ${port}`)
